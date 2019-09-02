48ffed454286d58034cce44860045c4df53e9151
Double Beep <38133098+double-beep@users.noreply.github.com>
2019-09-02 20:21:55 +0300

    Shorten up the comments
    Comments used HTTP which is no longer used per https://meta.stackexchange.com/q/292058

--- Natty/NattyReporter.user.js
+++ Natty/NattyReporter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Natty Reporter
 // @namespace    https://github.com/Tunaki/stackoverflow-userscripts
-// @version      0.26
+// @version      0.27
 // @description  Adds a Natty link below answers that sends a report for the bot in SOBotics. Intended to be used to give feedback on reports (true positive / false positive / needs edit) or report NAA/VLQ-flaggable answers.
 // @author       Tunaki
 // @include      /^https?:\/\/(www\.)?stackoverflow\.com\/.*/
@@ -123,41 +123,41 @@ const ScriptToInject = function() {
         '[add context around the link](//meta.stackexchange.com/a/8259) so your fellow users will ' +
         'have some idea what it is and why it’s there, then quote the most relevant part of the ' + 
         'page you\'re linking to in case the target page is unavailable. ' +
-        '[Answers that are little more than a link may be deleted.](//stackoverflow.com/help/deleted-answers)',
+        '[Answers that are little more than a link may be deleted.](/help/deleted-answers)',
       'naa <50':
         'This does not provide an answer to the question. You can [search for similar questions](//stackoverflow.com/search), ' +
         'or refer to the related and linked questions on the right-hand side of the page to find an answer. ' +
-        'If you have a related but different question, [ask a new question](//stackoverflow.com/questions/ask), ' + 
+        'If you have a related but different question, [ask a new question](/questions/ask), ' + 
         'and include a link to this one to help provide context. ' +
-        'See: [Ask questions, get answers, no distractions](//stackoverflow.com/tour)',
+        'See: [Ask questions, get answers, no distractions](/tour)',
       'naa >50':
         'This post doesn\'t look like an attempt to answer this question. Every post here is expected to be ' +
         'an explicit attempt to *answer* this question; if you have a critique or need a clarification of ' +
-        'the question or another answer, you can [post a comment](//stackoverflow.com/help/privileges/comment) ' + 
+        'the question or another answer, you can [post a comment](/help/privileges/comment) ' + 
         '(like this one) directly below it. Please remove this answer and create either a comment or a new question. ' +
-        'See: [Ask questions, get answers, no distractions](//stackoverflow.com/tour)',
+        'See: [Ask questions, get answers, no distractions](/tour)',
       'thanks <15':
         'Please don\'t add _"thanks"_ as answers. They don\'t actually provide an answer to the question, ' + 
-        'and can be perceived as noise by its future visitors. Once you [earn](http://meta.stackoverflow.com/q/146472) ' +
-        'enough [reputation](http://stackoverflow.com/help/whats-reputation), you will gain privileges to ' +
-        '[upvote answers](http://stackoverflow.com/help/privileges/vote-up) you like. This way future visitors of the question ' +
+        'and can be perceived as noise by its future visitors. Once you [earn](//meta.stackoverflow.com/q/146472) ' +
+        'enough [reputation](/help/whats-reputation), you will gain privileges to ' +
+        '[upvote answers](/help/privileges/vote-up) you like. This way future visitors of the question ' +
         'will see a higher vote count on that answer, and the answerer will also be rewarded with reputation points. ' +
-        'See [Why is voting important](http://stackoverflow.com/help/why-vote).',
+        'See [Why is voting important](/help/why-vote).',
       'thanks >15':
         'Please don\'t add _"thanks"_ as answers. They don\'t actually provide an answer to the question, ' +
         'and can be perceived as noise by its future visitors. ' +
-        'Instead, [upvote answers](http://stackoverflow.com/help/privileges/vote-up) you like. This way future visitors of the question ' +
+        'Instead, [upvote answers](/help/privileges/vote-up) you like. This way future visitors of the question ' +
         'will see a higher vote count on that answer, and the answerer will also be rewarded with reputation points. ' +
-        'See [Why is voting important](http://stackoverflow.com/help/why-vote).',
+        'See [Why is voting important](/help/why-vote).',
       'me too':
         'Please don\'t add *"Me too"* as answers. It doesn\'t actually provide an answer to the question. ' +
-        'If you have a different but related question, then [ask](//$SITEURL$/questions/ask) it ' +
+        'If you have a different but related question, then [ask](/questions/ask) it ' +
         '(reference this one if it will help provide context). If you\'re interested in this specific question, ' +
-        'you can [upvote](//stackoverflow.com/help/privileges/vote-up) it, leave a [comment](//stackoverflow.com/help/privileges/comment), ' +
-        'or start a [bounty](//stackoverflow.com/help/privileges/set-bounties) ' +
-        'once you have enough [reputation](//stackoverflow.com/help/whats-reputation).',
+        'you can [upvote](/help/privileges/vote-up) it, leave a [comment](/help/privileges/comment), ' +
+        'or start a [bounty](/help/privileges/set-bounties) ' +
+        'once you have enough [reputation](/help/whats-reputation).',
       'lib':
-        'Please don\'t just post some tool or library as an answer. At least demonstrate [how it solves the problem](http://meta.stackoverflow.com/a/251605) in the answer itself.'
+        'Please don\'t just post some tool or library as an answer. At least demonstrate [how it solves the problem](//meta.stackoverflow.com/a/251605) in the answer itself.'
     };
       
     e.preventDefault();
