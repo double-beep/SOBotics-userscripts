880b6a0dba55b39a0f53cb978593f424b3c08d62
Double Beep <38133098+double-beep@users.noreply.github.com>
2019-09-11 19:59:53 +0300

    Revert handleAnswers() function changes
    Now it works.

--- Natty/NattyReporter.user.js
+++ Natty/NattyReporter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Natty Reporter
 // @namespace    https://github.com/Tunaki/stackoverflow-userscripts
-// @version      0.3
+// @version      0.31
 // @description  Adds a Natty link below answers that sends a report for the bot in SOBotics. Intended to be used to give feedback on reports (true positive / false positive / needs edit) or report NAA/VLQ-flaggable answers.
 // @author       Tunaki
 // @include      /^https?:\/\/(www\.)?stackoverflow\.com\/.*/
@@ -257,44 +257,11 @@ const ScriptToInject = function() {
     $posts.each(function() {
       var $this = $(this);
       $this.append($('<span>').attr('class', 'lsep').html('|'));
-      console.log("Added; Reached L220");
-      var $dropdown = $('<dl>').css({
-        'margin': '0', 'z-index': '1',
-        'position': 'absolute',
-        'white-space': 'nowrap',
-        'background': '#FFF',
-        'padding': '2px',
-        'border': '1px solid #9fa6ad',
-        'box-shadow': '0 2px 4px rgba(36,39,41,0.3)',
-        'cursor': 'default'
-      }).hide();
-      $.each(['tp', 'fp', 'ne'], function(i, val) {
-        $dropdown.append($('<dd>').append(
-          $('<a>').css({
-            'display': 'block',
-            'margin-top': '3px',
-            'width': 'auto'
-          }).click(reportToNatty).text(val)))});
-      $dropdown.append($('<hr>').css({
-        'margin-bottom': '6.5px'
-      }));
-      $.each([
-        'link-only',
-        'naa',
-        'lib',
-        'thanks',
-        'me too'
-      ], function(i, val) {
-        $dropdown.append($('<dd>').append(
-          $('<a>').css({
-            'display': 'block',
-            'margin-top': '3px',
-            'width': 'auto'
-          }).click(shortcutClicked).text(val)));
-      });
-      $this.append($('<a>').attr('class', 'report-natty-link').html('Natty').hover(function() {
-        $dropdown.toggle();
-      }).append($dropdown));
+      var $dropdown = $('<dl>').css({ 'margin': '0', 'z-index': '1', 'position': 'absolute', 'white-space': 'nowrap', 'background': '#FFF', 'padding': '2px', 'border': '1px solid #9fa6ad', 'box-shadow': '0 2px 4px rgba(36,39,41,0.3)', 'cursor': 'default' }).hide();
+      $.each(['tp', 'fp', 'ne'], function(i, val) { $dropdown.append($('<dd>').append($('<a>').css({ 'display': 'block', 'margin-top': '3px', 'width': 'auto' }).click(reportToNatty).text(val))); });
+      $dropdown.append($('<hr>').css({'margin-bottom': '6.5px'}));
+      $.each(['link-only', 'naa', 'lib', 'thanks', 'me too'], function(i, val) { $dropdown.append($('<dd>').append($('<a>').css({ 'display': 'block', 'margin-top': '3px', 'width': 'auto' }).click(shortcutClicked).text(val))); });
+      $this.append($('<a>').attr('class', 'report-natty-link').html('Natty').hover(function() { $dropdown.toggle(); }).append($dropdown));
     });
   };
 
